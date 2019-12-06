8a6cca047a9169e200c505e2a5b2e56f15940497
Sreyas <sreyassreelal@gmail.com>
2019-12-06 18:55:08 +0500

    day 6 solutions

---
+++ day6/src/main.rs
@@ -0,0 +1,32 @@
+use std::collections::HashMap;
+
+fn trace_path<'a>(orbits: &'a HashMap<&str, &str>, start: &str) -> Vec<&'a str> {
+    let mut satetlite = start;
+    let mut store = Vec::new();
+    while let Some(sat) = orbits.get(satetlite) {
+        satetlite = sat;
+        store.push(*sat)
+    }
+    store
+}
+
+fn main() {
+    let inputs: Vec<&str> = include_str!("../input").split('\n').collect();
+
+    let mut orbits: HashMap<&str, &str> = HashMap::new();
+    for x in inputs.iter() {
+        let data: Vec<&str> = x.split(')').map(|x| x).collect();
+        orbits.insert(data[1], data[0]);
+    }
+
+    let ypos: Vec<&str> = trace_path(&orbits, "YOU");
+    let spos: Vec<&str> = trace_path(&orbits, "SAN");
+    for (x, mine) in ypos.iter().enumerate() {
+        for (y, santa) in spos.iter().enumerate() {
+            if mine == santa {
+                println!("distance {}", x + y);
+                return;
+            }
+        }
+    }
+}
